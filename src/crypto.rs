4292c6e682fc409fe6d9a52585500db1ec454e2b
harry <harrywstevens4@gmail.com>
2026-05-05 18:21:10 +0100

    implemented a from_number constructor for Crypto enum

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,3 +1,18 @@
+use std::default::Default;
+
 pub enum Crypto {
-	AESPassword(String), //password for decryption
+	AESPassword(String),
+}
+impl Default for Crypto {
+	fn default() -> Crypto {
+		Crypto::AESPassword(String::new())
+	}
+}
+impl Crypto {
+	pub fn from_number(val: u16, password: &str) -> Option<Crypto> {
+		match val {
+			0 => Some(Crypto::AESPassword(password.to_string())),
+			_ => None,
+		}
+	}
 }
