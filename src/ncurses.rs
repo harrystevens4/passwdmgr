c54bb3a4dc4a23ae9584eca4ce35af83131c6def
harry <harrywstevens4@gmail.com>
2026-05-11 17:49:34 +0100

    changed interface to use references and lifetimes to ensure no use after frees at compile time

--- src/ncurses.rs
+++ src/ncurses.rs
@@ -1,5 +1,4 @@
 use std::ffi::*;
-use std::ptr;
 
 pub type chtype = c_uint;
 pub type WindowPtr = *mut c_void;
@@ -16,24 +15,13 @@ unsafe extern "C" {
 }
 
 pub struct Ncurses {
-	stdscr: Window,
-	windows: Vec<Window>,
+	stdscr: WindowPtr,
 }
 
-#[derive(Clone,Copy)]
-pub struct Window(WindowPtr);
-
-impl From<WindowPtr> for Window {
-	fn from(v: WindowPtr) -> Window {
-		Window(v)
-	}
-}
-
-impl From<Window> for WindowPtr {
-	fn from(v: Window) -> WindowPtr {
-		let Window(ptr) = v;
-		ptr
-	}
+#[derive(Clone)]
+pub struct Window<'a> {
+	ptr: WindowPtr,
+	__ncurses: &'a Ncurses, //leverage the borrow checker to ensure all windows are destroyed before ncurses is deinitialised at compile time!
 }
 
 impl Ncurses {
@@ -41,43 +29,45 @@ impl Ncurses {
 		let stdscr = unsafe {initscr()};
 		unsafe {wrefresh(stdscr)};
 		Self {
-			stdscr: Window(stdscr),
-			windows: vec![],
+			stdscr,
 		}
 	}
-	pub fn stdscr(&self) -> Window {self.stdscr}
-	pub fn end(self) {unsafe {endwin()};}
-	pub fn newwin(&mut self, lines: usize, cols: usize, begin_y: usize, begin_x: usize) -> Window {
-		let window = unsafe {newwin(lines as c_int,cols as c_int,begin_y as c_int,begin_x as c_int)};
-		self.windows.push(window.into());
-		window.into()
+	pub fn stdscr(&self) -> Window<'_> {
+		Window {
+			ptr: self.stdscr,
+			__ncurses: &self,
+		}
 	}
-	pub fn delwin(&mut self, window: Window){
-		for i in 0..(self.windows.len()) {
-			if ptr::eq(window.as_ptr(),self.windows[i].as_ptr()) {
-				self.windows.remove(i);
-				return;
-			}
+	pub fn end(self) {} //drop self
+	pub fn newwin(&self, lines: usize, cols: usize, begin_y: usize, begin_x: usize) -> Window<'_> {
+		let window = unsafe {newwin(lines as c_int,cols as c_int,begin_y as c_int,begin_x as c_int)};
+		Window {
+			ptr: window,
+			__ncurses: &self
 		}
 	}
 }
 
 impl Drop for Ncurses {
 	fn drop(&mut self){
-		for window in self.windows.clone() {
-			self.delwin(window);
-		}
 		unsafe {endwin()};
 	}
 }
 
-impl Window {
-	pub fn as_ptr(&self) -> WindowPtr {(*self).into()}
-	pub fn refresh(&self) {unsafe {wrefresh((*self).into())};}
-	pub fn r#box(&self, verch: chtype, horch: chtype) {unsafe {wborder((*self).into(),verch,horch,0,0,0,0,0,0)};}
+impl Window<'_> {
+	pub fn as_ptr(&self) -> WindowPtr {self.ptr}
+	pub fn refresh(&self) {unsafe {wrefresh(self.as_ptr())};}
+	pub fn r#box(&self, verch: chtype, horch: chtype) {unsafe {wborder(self.as_ptr(),verch,horch,0,0,0,0,0,0)};}
 	pub fn getmaxyx(&self) -> (usize,usize) { //(y,x)
 		let x = unsafe {getmaxx(self.as_ptr())};
 		let y = unsafe {getmaxy(self.as_ptr())};
 		(y as usize,x as usize)
+	}
+	pub fn delwin(self) {} //drop self
+}
+
+impl Drop for Window<'_> {
+	fn drop(&mut self){
+		unsafe {delwin(self.as_ptr())};
 	}
 }
