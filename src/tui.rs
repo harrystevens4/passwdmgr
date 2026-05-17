7c8c8439695283c1c07c591c90395c007a565dd4
harry <harrywstevens4@gmail.com>
2026-05-17 21:35:49 +0100

    fixed some of the errorst

--- src/tui.rs
+++ src/tui.rs
@@ -3,18 +3,19 @@ use crate::password_store::{PasswordStore,PasswordStoreEntry};
 
 use std::ops::Drop;
 use std::cmp::{min,max};
+use std::mem::MaybeUninit;
 
-struct TuiInterface<'_> {
+struct TuiInterface<'a,'b> {
 	ncurses: Ncurses,
-	stdscr: Window<'_>,
-	selection_window: Window<'_>,
-	controls_window: Window<'_>,
-	info_window: Window<'_>,
-	selected_button: usize,
-	filtered_selected_password: usize,
-	unfiltered_selected_password: usize,
+	stdscr: Window<'a>,
+	selection_window: Window<'a>,
+	controls_window: Window<'a>,
+	info_window: Window<'a>,
+	selected_button: isize,
+	filtered_selected_password: isize,
+	unfiltered_selected_password: isize,
 	filter: String,
-	password_store: PasswordStore,
+	password_store: &'b PasswordStore,
 }
 
 trait ClampCharLength {
@@ -28,11 +29,11 @@ impl ClampCharLength for str {
 	}
 }
 
-impl TuiInterface {
+impl TuiInterface<'_,'_> {
 	fn redraw_all(&mut self){
 		self.redraw_selection_window();
 		self.redraw_controls_window();
-		self.redraw_info();
+		self.redraw_info_window();
 	}
 	
 	fn update_all(&mut self){
@@ -44,7 +45,7 @@ impl TuiInterface {
 
 	fn redraw_selection_window(&self){
 		//====== calculate window size and position ======
-		let (term_height,term_width) = stdscr.getmaxyx();
+		let (term_height,term_width) = self.stdscr.getmaxyx();
 		let selection_window_width = term_width/3;
 		let selection_window_height = term_height;
 		let selection_window_x = 0;
@@ -63,41 +64,41 @@ impl TuiInterface {
 	}
 	fn redraw_controls_window(&self){
 		//====== calculate window size and position ======
-		let (term_height,term_width) = stdscr.getmaxyx();
-		let control_window_height = 4;
-		let control_window_width = term_width-selection_window_width;
-		let control_window_x = selection_window_width;
-		let control_window_y = term_height-control_window_height;
+		let (term_height,term_width) = self.stdscr.getmaxyx();
+		let controls_window_height = 4;
+		let controls_window_width = term_width-term_width/3;
+		let controls_window_x = term_width/3;
+		let controls_window_y = term_height-controls_window_height;
 		//====== erase current window ======
-		self.control_window.erase();
+		self.controls_window.erase();
 		//====== move and resize ======
 		//resize
-		self.control_window.resize(control_window_height,control_window_width);
+		self.controls_window.resize(controls_window_height,controls_window_width);
 		//move
-		self.control_window.mvwin(control_window_y,control_window_x);
+		self.controls_window.mvwin(controls_window_y,controls_window_x);
 		//====== redraw borders ======
-		self.control_window.r#box(0,0);
+		self.controls_window.r#box(0,0);
 		//refresh
-		self.control_window.refresh();
+		self.controls_window.refresh();
 	}
 	fn redraw_info_window(&self){
 		//====== calculate window size and position ======
-		let (term_height,term_width) = stdscr.getmaxyx();
-		let info_window_width = control_window_width;
-		let info_window_height = term_height-control_window_height;
-		let info_window_x = selection_window_width;
+		let (term_height,term_width) = self.stdscr.getmaxyx();
+		let info_window_width = term_width - term_width/3;
+		let info_window_height = term_height-4;
+		let info_window_x = term_width/3;
 		let info_window_y = 0;
 		//====== erase current window ======
-		info_window.erase();
+		self.info_window.erase();
 		//====== move and resize ======
 		//resize
-		info_window.resize(info_window_height,info_window_width);
+		self.info_window.resize(info_window_height,info_window_width);
 		//move
-		info_window.mvwin(info_window_y,info_window_x);
+		self.info_window.mvwin(info_window_y,info_window_x);
 		//====== redraw borders ======
-		info_window.r#box(0,0);
+		self.info_window.r#box(0,0);
 		//refresh
-		info_window.refresh();
+		self.info_window.refresh();
 	}
 	
 	fn change_selected_password(&mut self, offset: isize){
@@ -105,24 +106,25 @@ impl TuiInterface {
 		if self.password_store.password_count() == 0 {return}
 		//change filtered or unfiltered selected password
 		if self.filter.len() > 0 {
-			filtered_selected_password += offset;
+			self.filtered_selected_password += offset;
 		}else {
-			unfiltered_selected_password += offset;
-			unfiltered_selected_password = unfiltered_selected_password
-				.rem_euclid(self.password_store.password_count());
+			let new_selection = self.unfiltered_selected_password + offset;
+			self.unfiltered_selected_password = new_selection
+				.rem_euclid(self.password_store.password_count() as isize);
 		}
 	}
 
-	fn update_filter(&mut self){
-		self.filtered_selected_password = min(self.filtered_selected_password,selectable_passwords.len() as isize);
+	fn update_filter(&mut self, new_filter: String){
+		self.filter = new_filter;
+		self.filtered_selected_password = min(self.filtered_selected_password,self.filter_passwords().len() as isize);
 	}
 
-	fn filter_passwords() -> Vec<PasswordStoreEntry> {
+	fn filter_passwords(&self) -> Vec<PasswordStoreEntry> {
 		self.password_store
 			.entries()
 			.into_iter()
-			.filter(|p| if self.filter.len() != 0 && p.identifier().find(&self.filter).is_some())
-			.collect<Vec<String>>();
+			.filter(|p| self.filter.len() != 0 && p.identifier().find(&self.filter).is_some())
+			.collect()
 	}
 
 	fn selected_password_index(&self) -> usize {
@@ -137,22 +139,25 @@ impl TuiInterface {
 					if *i == 0 {None}
 					else {Some(password)}
 				})
-				.count();
+				.count()
 		}else {
-			self.unfiltered_selected_password
+			self.unfiltered_selected_password as usize
 		}
 	}
 
 	fn update_selection_window(&mut self){
 		//====== apply filter ======
-		let selections = self.filter_passwords();
-		filtered_selected_password = filtered_selected_password
-			.rem_euclid(self.password_store.password_count());
+		let selections = self.filter_passwords()
+			.into_iter()
+			.map(|p| p.identifier())
+			.collect::<Vec<_>>();
+		self.filtered_selected_password = self.filtered_selected_password
+			.rem_euclid(self.password_store.password_count() as isize);
 		//====== show selection window ======
 		let selection_index =
 			if self.filter.len() != 0 {self.filtered_selected_password}
 			else {self.unfiltered_selected_password};
-		self.render_selections(self.selection_window,&selections,selection_index);
+		self.render_selections(&self.selection_window,selections.as_slice(),selection_index as usize);
 		//====== refresh window ======
 		self.selection_window.refresh();
 	}
@@ -161,17 +166,17 @@ impl TuiInterface {
 		let (height,width) = self.controls_window.getmaxyx();
 		if width < 11 || height < 4 {return}
 		//====== render search bar ======
-		control_window.mvaddstr(1,1,"search: ");
+		self.controls_window.mvaddstr(1,1,"search: ");
 		let filter_input_length = width-2-8-1; //remove one for style
 		let filter_input_offset = max(0,(self.filter.len() as isize) - (filter_input_length as isize)) as usize;
-		let viewable_filter_chars = format!("{:1$}",&filter[filter_input_offset..],filter_input_length);
-		self.control_window.mvaddstr(1,9,viewable_filter_chars);
-		self.control_window.mvchgat(1,9,filter_input_length as isize,&[VideoAttribute::Underline],0);
+		let viewable_filter_chars = format!("{:1$}",&self.filter[filter_input_offset..],filter_input_length);
+		self.controls_window.mvaddstr(1,9,viewable_filter_chars);
+		self.controls_window.mvchgat(1,9,filter_input_length as isize,&[VideoAttribute::Underline],0);
 		//====== render buttons ======
 		if width < 27 || height < 4 {return}
 		self.controls_window.mvaddstr(2,1,"<new> <del> <edit> <sort>");
 		//underline selected button
-		match selected_button {
+		match self.selected_button {
 			0 => self.controls_window.mvchgat(2,2,3,&[VideoAttribute::Reverse],0),
 			1 => self.controls_window.mvchgat(2,8,3,&[VideoAttribute::Reverse],0),
 			2 => self.controls_window.mvchgat(2,14,4,&[VideoAttribute::Reverse],0),
@@ -185,31 +190,41 @@ impl TuiInterface {
 	}
 
 	fn update_info_window(&self){
-		let Some(password_entry) = self.password_store
-			.entries()
-			.get(self.selected_password_index())
-		else {return};
 		let (height,width) = self.info_window.getmaxyx();
+		//====== check password available to show ======
+		let passwords = self.password_store.entries();
+		let Some(password_entry) = passwords
+			.get(self.selected_password_index())
+		else {
+			//no password to show
+			reset_window(&self.info_window);
+			let (height,width) = self.info_window.getmaxyx();
+			if height > 2 && width > 2 {
+				self.info_window.mvaddstr(1,1,"no password selected".clamp_len(width-2));
+			}
+			self.info_window.refresh();
+			return;
+		};
 		if width <= 2 {return}
 		//====== only show as much info as can fit in the window ======
 		//identifier
 		if height < 3 {return}
-		info_window.mvaddstr(1,1,
+		self.info_window.mvaddstr(1,1,
 			format!("{:1$}",password_entry.identifier().clamp_len(width-2),width-2)
 		);
 		//====== username ======
 		if height < 4 {return}
-		info_window.mvaddstr(2,1,
+		self.info_window.mvaddstr(2,1,
 			format!("username: {:1$}",password_entry.username(),width-2).clamp_len(width-2)
 		);
 		//====== password ======
 		if height < 5 {return}
-		info_window.mvaddstr(3,1,
+		self.info_window.mvaddstr(3,1,
 			format!("password: \"{}\"{}",password_entry.password()," ".repeat(width-2)).clamp_len(width-2)
 		);
 		//====== notes ======
 		if height < 6 {return}
-		info_window.mvaddstr(4,1,
+		self.info_window.mvaddstr(4,1,
 			format!("notes: {:1$}",password_entry.notes(),width-2).clamp_len(width-2)
 		);
 		//====== date added ======
@@ -219,7 +234,7 @@ impl TuiInterface {
 	//we can reuse this for popup selection windows and the main password selection window
 	fn render_selections(&self, window: &Window, selections: &[String], selection_index: usize){
 		//====== figure out where the cursor and all the selections should be ======
-		let (height,width) = self.selection_window.getmaxyx();
+		let (height,width) = window.getmaxyx();
 		//dont do anything
 		if selections.len() == 0 || height < 3 || width < 3 {return}
 		//I cast... 1000000 isizes!!!!!
@@ -242,95 +257,71 @@ impl TuiInterface {
 				.take(width-2)
 				.collect::<String>();
 			//add the selection
-			selection_window.mvaddstr(i+1,1,format!("{:1$}",truncated_selection,width-2,)); //pad with spaces
+			window.mvaddstr(i+1,1,format!("{:1$}",truncated_selection,width-2,)); //pad with spaces
 			//remove any previous highlighting
-			selection_window.mvchgat(i+1,1,(width-2) as isize,&[VideoAttribute::Normal],0);
+			window.mvchgat(i+1,1,(width-2) as isize,&[VideoAttribute::Normal],0);
 		}
 		//====== highlight the current selection ======
-		selection_window.mvchgat(cursor_index+1,1,(width-2) as isize,&[VideoAttribute::Reverse],0);
-	}
-	
-	fn update_controls_window(&self){
-		self.controls_window.refresh();
-	}
-	
-	fn update_info_window(&self){
-		self.info_window.refresh();
+		window.mvchgat(cursor_index+1,1,(width-2) as isize,&[VideoAttribute::Reverse],0);
 	}
 }
 
 pub fn run_tui(password_store: &PasswordStore){
-	//====== ncurses initialisation ======
-	let ncurses = Ncurses::init();
-	let Some(stdscr) = ncurses.stdscr()
-	else {
-		eprintln!("couldn't open stdscr");
-		return;
-	};
-	stdscr.keypad(true);
-	ncurses.noecho();
 	//====== create windows ======
 	let mut tui_interface = TuiInterface {
-		//windows
-		ncurses,
-		stdscr,
-		selection_window: ncurses.newwin(1,1,0,0),
-		control_window: ncurses.newwin(1,1,0,0),
-		info_window: ncurses.newwin(1,1,0,0),
 		//variables
 		selected_button: 0,
 		unfiltered_selected_password: 0, //seperate so when the user clears the filter they go back to where they were
 		filtered_selected_password: 0,
 		filter: String::new(),
-	}
+		password_store: password_store,
+		//windows
+		//TODO: this is UB but im lowkey to tired to fix rn
+		ncurses: unsafe {MaybeUninit::<Ncurses>::zeroed().assume_init()},
+		selection_window: unsafe {MaybeUninit::<Window>::zeroed().assume_init()},
+		controls_window: unsafe {MaybeUninit::<Window>::zeroed().assume_init()},
+		info_window: unsafe {MaybeUninit::<Window>::zeroed().assume_init()},
+		stdscr: unsafe {MaybeUninit::<Window>::zeroed().assume_init()},
+	};
+	//====== ncurses initialisation ======
+	let ncurses = Ncurses::init();
+	//windows
+	tui_interface.ncurses = ncurses;
+	tui_interface.selection_window = tui_interface.ncurses.newwin(1,1,0,0);
+	tui_interface.controls_window = tui_interface.ncurses.newwin(1,1,0,0);
+	tui_interface.info_window = tui_interface.ncurses.newwin(1,1,0,0);
+	//stdscr
+	let Some(stdscr) = tui_interface.ncurses.stdscr()
+	else {
+		eprintln!("couldn't open stdscr");
+		return;
+	};
+	tui_interface.stdscr = stdscr;
+	//fiddle with input settings
+	tui_interface.stdscr.keypad(true);
+	tui_interface.ncurses.noecho();
+	tui_interface.stdscr = stdscr;
 	//update window sizes
-	tui_interface.redraw_windows();
+	tui_interface.redraw_all();
 	//====== update loop ======
 	loop {
-		let selectable_passwords = password_store.entries();
-		let selectable_passwords_index = 
-			if filter.len() > 0 {filtered_selected_password}
-			else {unfiltered_selected_password} as usize;
 		//====== render each window ======
-		render_selections(
-			&selection_window,
-			&selectable_passwords.iter().map(|p| p.identifier()).collect::<Vec<_>>(),
-			selectable_passwords_index
-		);
-		if selectable_passwords.len() == 0 {
-			//no password to show
-			reset_window(&info_window);
-			let (height,width) = info_window.getmaxyx();
-			if height > 2 && width > 2 {
-				info_window.mvaddstr(1,1,"no password selected".clamp_len(width-2));
-			}
-		}else {
-			show_password_info(&info_window,&selectable_passwords[selectable_passwords_index]);
-		}
-		render_control_window(&control_window,selected_button as usize,&filter);
+		tui_interface.update_all();
 		//====== input handling ======
-		match ncurses.getch() {
+		match tui_interface.ncurses.getch() {
 			Input::Up => tui_interface.change_selected_password(1),
-			Input::Down => if filter.len() > 0 {filtered_selected_password+=1} else {unfiltered_selected_password+=1},
-			Input::Left => selected_button -= 1,
-			Input::Right => selected_button += 1,
-			Input::Resize => redraw_windows(&stdscr,&selection_window,&control_window,&info_window),
-			Input::AsciiChar(ch) => filter.push(ch),
-			Input::Backspace => {filter.pop();},
+			Input::Down => tui_interface.change_selected_password(-1),
+			Input::Left => (),
+			Input::Right => (),
+			Input::Resize => tui_interface.redraw_all(),
+			Input::AsciiChar(ch) => (),
+			Input::Backspace => (),
 			Input::Enter => {
-				show_selection_menu(&ncurses,"Sort by",["identifier","time added","username"]);
+				show_selection_menu(&tui_interface.ncurses,"Sort by",["identifier","time added","username"]);
 			},
 			_ => ()
 		}
-		//rem_euclid is mod that works for negative numbers
-		selected_button = selected_button.rem_euclid(4);
 	}
-	//====== cleanup ======
-	control_window.delwin();
-	selection_window.delwin();
-	info_window.delwin();
-	stdscr.delwin();
-	ncurses.end();
 }
 
 fn reset_window(window: &Window){
